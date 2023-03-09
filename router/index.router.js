efdb65ba429501b0702a9dfe18cd9372f3f1052f
your_username <atiwattum1@gmail.com>
2023-03-09 16:43:30 +0700

    'deploy'

--- router/index.router.js
+++ router/index.router.js
@@ -6,11 +6,11 @@ var router = express.Router();
 const auth = require('./auth.router');
 router.use('/auth', auth);
 
-// const bills = require('./bill.router');
-// router.use('/bill', bills);
+const bills = require('./bill.router');
+router.use('/bill', bills);
 
-// const products = require('./products.router');
-// router.use('/product', products);
+const products = require('./products.router');
+router.use('/product', products);
 
 const topping = require('./topping.router');
 router.use('/topping', topping);
