276bb0e138eb3d0b3b6a4623ee093c33fd75f65b
your_username <atiwattum1@gmail.com>
2023-03-09 16:53:12 +0700

    'deploy'

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -2,7 +2,7 @@ const config = require('./config')
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.NODE_ENV != 'development' ? config.mongo_db: process.env.MONGO_DEV , { useUnifiedTopology: true, useNewUrlParser: true });
-
+console.log(process.env.NODE_ENV != 'development' ? config.mongo_db: process.env.MONGO_DEV);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.on('connected', function() {
