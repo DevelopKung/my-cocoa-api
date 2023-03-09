8bbd22937e094d197fa436fe665b4fcf24e43254
your_username <atiwattum1@gmail.com>
2023-03-09 16:54:23 +0700

    'deploy'

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = {
   server_cert_file: process.env.SSL_KEY_FILE,
   server_api_prefix_path: process.env.API_PREFIX_PATH || '',
   jwt_secret: process.env.JWT_SECRET || 'jwt_secret_key',
-  mongo_db: process.env.MONGO,
+  mongo_db: process.env.MONGO_DB,
   allow_cors_url: process.env.ALLOW_CORS_URL,
   url_api: process.env.URL_API
 }
