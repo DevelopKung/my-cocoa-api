ef305c794fc2b568651729bfd7d45a05cda4d38d
your_username <atiwattum1@gmail.com>
2023-03-17 16:37:53 +0700

    'deploy'

--- controller/service.controller.js
+++ controller/service.controller.js
@@ -1,9 +1,13 @@
 const CYCLIC_BUCKET_NAME = process.env.CYCLIC_BUCKET_NAME;
-const fs = require('@cyclic.sh/s3fs')(CYCLIC_BUCKET_NAME)
+const fs = require('@cyclic.sh/s3fs')
 const config = require('./../config/path-image')
-const pathImage = config.full_path_image+'/products'
+const pathProducts = config.full_path_image+'/products'
+const pathTopping = config.full_path_image+'/topping'
 const path = require('path')
-
+const { promisify } = require('util')
+const readDir = promisify(fs.readdir)
+const readFile = promisify(fs.readFile)
+let DirFile = path.join(__dirname, "../")
 module.exports = {
   create: async(req, res) => {
     let file = req.file
@@ -13,14 +17,18 @@ module.exports = {
       fs.mkdirSync(config.full_path_image, { recursive: true });
     }
 
-    if (!fs.existsSync(pathImage)) {
-      fs.mkdirSync(pathImage, { recursive: true });
+    if (!fs.existsSync(pathProducts)) {
+      fs.mkdirSync(pathProducts, { recursive: true });
+    }
+
+    if (!fs.existsSync(pathTopping)) {
+      fs.mkdirSync(pathTopping, { recursive: true });
     }
     
     let newFileName = `${file.originalname.split('.')[0]}-${Date.now()}`
     let fileName = file.originalname.split('.')[0]
     let fileExt = file.originalname.split('.')[1]
-    let newPath = `${pathImage}/${newFileName}.${fileExt}`
+    let newPath = `${pathProducts}/${newFileName}.${fileExt}`
     let buffer = file.buffer
     await fs.writeFile(newPath, buffer, async(err) => { if (err) return err; })
 
@@ -31,14 +39,18 @@ module.exports = {
       newPath: config.url_full_path_image + '/products/' + newFileName + '.' + fileExt,
       path: config.full_path_image,
       checkPath: fs.existsSync(config.full_path_image),
-      pathImage: pathImage,
-      checkPathImage: fs.existsSync(pathImage)
+      pathProducts: pathProducts,
+      checkPathImage: fs.existsSync(pathProducts),
+      pathTmp: path.join(__dirname, "../../"),
+      pathVar: await readDir(DirFile),
+      checkPathTmp: fs.existsSync(fs.existsSync(__dirname, "../../tmp")),
     }
 
     res.status(200).send({ status: true, message: "success", payload: data });
 
     setTimeout(() => {
       if (fs.existsSync(newPath)) fs.unlinkSync(newPath);
+      fs.rmSync(`${pathProducts}`, { recursive: true });
     }, 10000);
   },
 }
