eb7a995c7a20f08867674f56e338266eb45dfe67
your_username <atiwattum1@gmail.com>
2023-03-17 16:53:48 +0700

    'deploy'

--- controller/service.controller.js
+++ controller/service.controller.js
@@ -7,51 +7,54 @@ const pathTopping = config.full_path_image+'/topping'
 const path = require('path')
 const { promisify } = require('util')
 const readDir = promisify(fs.readdir)
-const readFile = promisify(fs.readFile)
+// const readFile = promisify(fs.readFile)
 let DirFile = path.join(__dirname, "../")
 module.exports = {
   create: async(req, res) => {
-    let file = req.file
-    let form = req.body
+    let path = await DirFile(DirFile)
+    res.status(200).send(path)
+    return
+    // let file = req.file
+    // let form = req.body
     
-    if (!fs.existsSync(config.full_path_image)) {
-      fs.mkdirSync(config.full_path_image, { recursive: true });
-    }
+    // if (!fs.existsSync(config.full_path_image)) {
+    //   fs.mkdirSync(config.full_path_image, { recursive: true });
+    // }
 
-    if (!fs.existsSync(pathProducts)) {
-      fs.mkdirSync(pathProducts, { recursive: true });
-    }
+    // if (!fs.existsSync(pathProducts)) {
+    //   fs.mkdirSync(pathProducts, { recursive: true });
+    // }
 
-    if (!fs.existsSync(pathTopping)) {
-      fs.mkdirSync(pathTopping, { recursive: true });
-    }
+    // if (!fs.existsSync(pathTopping)) {
+    //   fs.mkdirSync(pathTopping, { recursive: true });
+    // }
     
-    let newFileName = `${file.originalname.split('.')[0]}-${Date.now()}`
-    let fileName = file.originalname.split('.')[0]
-    let fileExt = file.originalname.split('.')[1]
-    let newPath = `${pathProducts}/${newFileName}.${fileExt}`
-    let buffer = file.buffer
-    await fs.writeFile(newPath, buffer, async(err) => { if (err) return err; })
+    // let newFileName = `${file.originalname.split('.')[0]}-${Date.now()}`
+    // let fileName = file.originalname.split('.')[0]
+    // let fileExt = file.originalname.split('.')[1]
+    // let newPath = `${pathProducts}/${newFileName}.${fileExt}`
+    // let buffer = file.buffer
+    // await fs.writeFile(newPath, buffer, async(err) => { if (err) return err; })
 
-    let data = {
-      name: fileName,
-      newName: newFileName,
-      ext: fileExt,
-      newPath: config.url_full_path_image + '/products/' + newFileName + '.' + fileExt,
-      path: config.full_path_image,
-      checkPath: fs.existsSync(config.full_path_image),
-      pathProducts: pathProducts,
-      checkPathImage: fs.existsSync(pathProducts),
-      pathTmp: path.join(__dirname, "../../"),
-      pathVar: await readDir(DirFile)
-    }
+    // let data = {
+    //   name: fileName,
+    //   newName: newFileName,
+    //   ext: fileExt,
+    //   newPath: config.url_full_path_image + '/products/' + newFileName + '.' + fileExt,
+    //   path: config.full_path_image,
+    //   checkPath: fs.existsSync(config.full_path_image),
+    //   pathProducts: pathProducts,
+    //   checkPathImage: fs.existsSync(pathProducts),
+    //   pathTmp: path.join(__dirname, "../../"),
+    //   pathVar: await readDir(DirFile)
+    // }
 
-    res.status(200).send({ status: true, message: "success", payload: data });
+    // res.status(200).send({ status: true, message: "success", payload: data });
 
-    setTimeout(() => {
-      if (fs.existsSync(newPath)) fs.unlinkSync(newPath);
-      if (fs.existsSync(pathProducts)) fs.rmSync(`${pathProducts}`, { recursive: true });
-      if (fs.existsSync(pathTopping)) fs.rmSync(`${pathTopping}`, { recursive: true });
-    }, 10000);
+    // setTimeout(() => {
+    //   if (fs.existsSync(newPath)) fs.unlinkSync(newPath);
+    //   if (fs.existsSync(pathProducts)) fs.rmSync(`${pathProducts}`, { recursive: true });
+    //   if (fs.existsSync(pathTopping)) fs.rmSync(`${pathTopping}`, { recursive: true });
+    // }, 10000);
   },
 }
